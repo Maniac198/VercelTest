64af52f0d5aac0f528bb5b67dec10188610f3167
Maniac198 <mani.shc1908@gmail.com>
2024-08-25 21:52:37 +0530

    Vercel Deployment

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const home = require("./routes/home");
+
+// Middlewares
+const app = express();
+app.use(express.json());
+
+// Routes
+app.use("/home", home);
+
+
+app.get("/", (req,res) => {
+  res.send("<h1>Hello Vercel</h1>")
+})
+
+// Start the server and listen on port 3000
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
